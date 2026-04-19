3f8c86ddcbab033e644a66bbaf0131c23031c9db
askarmin17 <hpb30227@gmail.com>
2026-04-19 10:20:09 +0700

    Update: Menambahkan Footer Handyman dan memperbarui Layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
               </svg>
             </div>
-            <span className="text-xl font-bold text-white tracking-tight">ZoeMarket</span>
+            <span className="text-xl font-bold text-white tracking-tight">ClothesByAtuu</span>
           </div>
 
           {/* Desktop Menu */}
